989bfe309714df62862bd7d29c534df71b596eea
Diego Pacheco <diego.pacheco020603@gmail.com>
2021-06-08 15:32:04 -0600

    Configuracion inicial

---
+++ src/containers/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <h1>Hola niño</h1>
+    )
+}
+
+export default Home;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+//Espacio para REDUX
+
+//ROUTES
+import App from './routes/App';
+
+ReactDom.render(
+    <App/>,
+    document.getElementById('app')
+)
